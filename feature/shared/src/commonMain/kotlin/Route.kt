5d181fb0cc20c2268c6b64585bfef5656e2ddf1c
Kiotel <valera.lihenko@gmail.com>
2026-01-27 15:32:47 +0300

    Очистка кода

--- feature/shared/src/commonMain/kotlin/Route.kt
+++ feature/shared/src/commonMain/kotlin/Route.kt
@@ -3,25 +3,25 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface Route: NavKey {
-    @kotlinx.serialization.Serializable
+    @Serializable
     data object OnBoarding: Route {
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object Greeting: Route
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object DescriptionForm: Route
     }
-    @kotlinx.serialization.Serializable
+    @Serializable
     data object Auth: Route {
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object Login: Route
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object Registration: Route
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object PasswordReset: Route
     }
-    @kotlinx.serialization.Serializable
+    @Serializable
     data object Error: Route{
-        @kotlinx.serialization.Serializable
+        @Serializable
         data object UnknownError: Route
     }
 }
