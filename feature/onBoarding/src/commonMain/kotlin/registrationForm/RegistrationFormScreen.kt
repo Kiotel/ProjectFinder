e01c82a2091fad437950bf792eb88cb4548a8052
valera-androidStudio <valera.lihenko@gmail.com>
2026-03-18 18:45:39 +0300

    Доделка экрана регистрации

--- feature/onBoarding/src/commonMain/kotlin/registrationForm/RegistrationFormScreen.kt
+++ feature/onBoarding/src/commonMain/kotlin/registrationForm/RegistrationFormScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -18,18 +17,21 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,6 +44,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -52,6 +55,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import components.BrandTitle
 import dev.chrisbanes.haze.materials.ExperimentalHazeMaterialsApi
+import dev.chrisbanes.haze.rememberHazeState
 import modifiers.glassEffect
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.stringResource
@@ -88,7 +92,7 @@ internal fun RegistrationFormScreen(
             modifier = Modifier.verticalScroll(scrollState).fillMaxSize()
                 .padding(top = innerPadding.calculateTopPadding() + 36.dp).imePadding(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(36.dp)
+            verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
             BrandTitle()
             RegistrationForm(
@@ -140,80 +144,88 @@ private fun RegistrationForm(
 
     val focusManager = LocalFocusManager.current
 
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+    Box(
+        modifier = modifier,
     ) {
-        Text(
-            text = "Регистрация",
-
-            modifier = Modifier.padding(top = 24.dp),
-            style = MaterialTheme.typography.displaySmallEmphasized
-        )
-        RegistrationTextInput(
-            errorText = data.emailErrorText,
-            isError = !data.emailErrorText.isNullOrBlank(),
-            labelText = "Почта",
-            value = data.email,
-            onValueChange = { onEmailChange(it) },
-            keyboardType = KeyboardType.Email,
-            onNext = { loginFocus.requestFocus(focusDirection = FocusDirection.Up) },
-        )
-        RegistrationTextInput(
-            errorText = data.loginErrorText,
-            isError = !data.loginErrorText.isNullOrBlank(),
-            labelText = "Логин",
-            value = data.login,
-            onValueChange = { onLoginChange(it) },
-            onNext = { passwordFocus.requestFocus(focusDirection = FocusDirection.Up) },
-            focusRequester = loginFocus
-        )
-        RegistrationTextInput(
-            isSecret = true,
-            errorText = data.passwordErrorText,
-            isError = !data.passwordErrorText.isNullOrBlank(),
-            labelText = "Пароль",
-            value = data.password,
-            onValueChange = { onPasswordChange(it) },
-            keyboardType = KeyboardType.Password,
-            onNext = { passwordCopyFocus.requestFocus(focusDirection = FocusDirection.Up) },
-            focusRequester = passwordFocus
-        )
-        RegistrationTextInput(
-            isSecret = true,
-            errorText = data.passwordCopyErrorText,
-            isError = !data.passwordCopyErrorText.isNullOrBlank(),
-            labelText = "Повторите пароль",
-            value = data.passwordCopy,
-            onValueChange = { onPasswordCopyChange(it) },
-            keyboardType = KeyboardType.Password,
-            imeAction = ImeAction.Done,
-            onNext = { focusManager.clearFocus() },
-            focusRequester = passwordCopyFocus
-        )
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Checkbox(
-                checked = data.consent, onCheckedChange = { onConsentChange(it) })
+            Text(
+                text = "Регистрация",
+                modifier = Modifier.padding(top = 18.dp),
+                style = MaterialTheme.typography.titleLargeEmphasized,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.ExtraBold
+            )
+            RegistrationTextInput(
+                errorText = data.emailErrorText,
+                isError = !data.emailErrorText.isNullOrBlank(),
+                labelText = "Почта",
+                value = data.email,
+                onValueChange = { onEmailChange(it) },
+                keyboardType = KeyboardType.Email,
+                onNext = { loginFocus.requestFocus(focusDirection = FocusDirection.Up) },
+            )
+            RegistrationTextInput(
+                errorText = data.loginErrorText,
+                isError = !data.loginErrorText.isNullOrBlank(),
+                labelText = "Логин",
+                value = data.login,
+                onValueChange = { onLoginChange(it) },
+                onNext = { passwordFocus.requestFocus(focusDirection = FocusDirection.Up) },
+                focusRequester = loginFocus
+            )
+            RegistrationTextInput(
+                isSecret = true,
+                errorText = data.passwordErrorText,
+                isError = !data.passwordErrorText.isNullOrBlank(),
+                labelText = "Пароль",
+                value = data.password,
+                onValueChange = { onPasswordChange(it) },
+                keyboardType = KeyboardType.Password,
+                onNext = { passwordCopyFocus.requestFocus(focusDirection = FocusDirection.Up) },
+                focusRequester = passwordFocus
+            )
+            RegistrationTextInput(
+                isSecret = true,
+                errorText = data.passwordCopyErrorText,
+                isError = !data.passwordCopyErrorText.isNullOrBlank(),
+                labelText = "Повторите пароль",
+                value = data.passwordCopy,
+                onValueChange = { onPasswordCopyChange(it) },
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done,
+                onNext = { focusManager.clearFocus() },
+                focusRequester = passwordCopyFocus
+            )
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = data.consent, onCheckedChange = { onConsentChange(it) })
 
-            Column {
-                Text(
-                    text = "Я согласен на обработку персональных данных.",
-                    style = MaterialTheme.typography.labelSmall,
-                    textAlign = TextAlign.Center
-                )
-                AgreementLink(link = "https://example.com")
+                Column {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Согласие на обработку персональных данных.",
+                        style = MaterialTheme.typography.labelSmall,
+                        textAlign = TextAlign.Start
+                    )
+                    AgreementLink(link = "https://example.com")
+                }
+
             }
-
-        }
-        FilledTonalButton(
-            modifier = Modifier.padding(bottom = 24.dp),
-            onClick = onRegistration,
-        ) {
-            Text(text = "Зарегистрироваться", style = MaterialTheme.typography.labelLarge)
+            Button(
+                modifier = Modifier.padding(bottom = 24.dp),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 2.dp,
+                ),
+                onClick = onRegistration,
+            ) {
+                Text(text = "Зарегистрироваться", style = MaterialTheme.typography.labelLarge)
+            }
         }
     }
 }
@@ -234,54 +246,78 @@ private fun RegistrationTextInput(
     isSecret: Boolean = false
 ) {
     var hide by remember { mutableStateOf(true) }
+    val visualTransformation =
+        if (hide && isSecret) PasswordVisualTransformation() else VisualTransformation.None
 
-    val visualTransformation = if (hide && isSecret) {
-        PasswordVisualTransformation()
-    } else VisualTransformation.None
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
+    ) {
+        if (labelText != null) {
+            Text(
+                text = labelText,
+                style = MaterialTheme.typography.labelLargeEmphasized,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.padding(start = 4.dp)
+            )
+        }
 
-    Box(modifier = modifier.requiredHeight(80.dp)) {
-        OutlinedTextField(
-            visualTransformation = visualTransformation,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = keyboardType, imeAction = imeAction
-            ),
-            keyboardActions = KeyboardActions(onNext = { onNext() }, onDone = { onNext() }),
-            trailingIcon = {
-                if (isSecret) {
-                    IconButton(
-                        onClick = {
-                            hide = !hide
-                        }) {
-                        Icon(
-                            if (hide) Icons.Default.Visibility else Icons.Default.VisibilityOff,
-                            contentDescription = null
-                        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .glassEffect(
+                    hazeState = LocalHazeState.current,
+                    blurRadius = 10.dp,
+                    shape = MaterialTheme.shapes.medium,
+                    fillAlpha = 0.2f
+                )
+        ) {
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
+                visualTransformation = visualTransformation,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = keyboardType,
+                    imeAction = imeAction
+                ),
+                keyboardActions = KeyboardActions(onNext = { onNext() }, onDone = { onNext() }),
+                placeholder = {
+                    if (placeHolderText != null) {
+                        Text(text = placeHolderText, style = MaterialTheme.typography.bodyMedium)
                     }
-                }
-            },
-            supportingText = {
-                Text(
-                    text = if (isError) errorText ?: "" else " ",
-                    style = MaterialTheme.typography.labelMedium
+                },
+                trailingIcon = {
+                    if (isSecret) {
+                        IconButton(onClick = { hide = !hide }) {
+                            Icon(
+                                if (hide) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                },
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    errorContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
                 )
-            },
-            placeholder = {
-                Text(
-                    text = placeHolderText ?: "", style = MaterialTheme.typography.labelMedium
-                )
-            },
-            label = {
-                if (labelText != null) {
-                    Text(
-                        text = labelText, style = MaterialTheme.typography.labelMedium
-                    )
-                }
-            },
-            isError = isError,
-            modifier = Modifier.focusRequester(focusRequester),
-            value = value,
-            onValueChange = onValueChange,
-            singleLine = true,
+            )
+        }
+
+        Text(
+            text = if (isError) errorText ?: "" else "",
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.error,
+            modifier = Modifier.padding(start = 4.dp)
         )
     }
 }
@@ -291,8 +327,11 @@ private fun RegistrationTextInput(
 )
 @Composable
 private fun RegistrationTextInputPreview() {
-    RegistrationTextInput(
-        value = "Preview", onValueChange = {})
+    val hazeState = rememberHazeState()
+    CompositionLocalProvider(LocalHazeState provides hazeState) {
+        RegistrationTextInput(
+            value = "Preview", onValueChange = {})
+    }
 }
 
 @Preview(
@@ -300,25 +339,28 @@ private fun RegistrationTextInputPreview() {
 )
 @Composable
 private fun RegistrationFormPreview() {
-    val registrationData = RegistrationFormData(
-        email = "PreviewEmail",
-        login = "PreviewLogin",
-        password = "PreviewPassword",
-        passwordCopy = "PreviewPasswordCopy",
-        consent = true,
-        emailErrorText = null,
-        loginErrorText = null,
-        passwordErrorText = null,
-        passwordCopyErrorText = null,
-        consentErrorText = null,
-        registrationErrorText = null
-    )
-    RegistrationForm(
-        data = registrationData,
-        onEmailChange = {},
-        onLoginChange = {},
-        onPasswordChange = {},
-        onPasswordCopyChange = {},
-        onConsentChange = {},
-        onRegistration = {})
+    val hazeState = rememberHazeState()
+    CompositionLocalProvider(LocalHazeState provides hazeState) {
+        val registrationData = RegistrationFormData(
+            email = "PreviewEmail",
+            login = "PreviewLogin",
+            password = "PreviewPassword",
+            passwordCopy = "PreviewPasswordCopy",
+            consent = true,
+            emailErrorText = null,
+            loginErrorText = null,
+            passwordErrorText = null,
+            passwordCopyErrorText = null,
+            consentErrorText = null,
+            registrationErrorText = null
+        )
+        RegistrationForm(
+            data = registrationData,
+            onEmailChange = {},
+            onLoginChange = {},
+            onPasswordChange = {},
+            onPasswordCopyChange = {},
+            onConsentChange = {},
+            onRegistration = {})
+    }
 }
